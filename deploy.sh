78bd4dc038ce82796bb0819baf9687e8cc713670
iocn-io <iocn2x2y@gmail.com>
2018-06-12 15:17:21 +0800

    Deployed Site on 2018-06-12 15:17:05

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+curpath=`pwd`
+echo "Path is $curpath"
+cd ../iocn-io.github.io
+git pull
+ls | xargs rm -rf
+cp -r ../site/public/* .
+git config user.name "iocn-io"
+git config user.email "iocn2x2y@gmail.com"
+git add .
+curtime=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "Site Updated on $curtime"
+git push
+
+cd $curpath
+git config user.name "iocn-io"
+git config user.email "iocn2x2y@gmail.com"
+git add .
+git commit -m "Deployed Site on $curtime"
+git push
